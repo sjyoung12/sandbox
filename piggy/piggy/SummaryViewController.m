5c2a73745c4972162079496da268abe010c09292
Steve Young <stephen@shopkick.com>
2016-09-09 15:19:23 -0700

    Add .xib files for VCs

--- piggy/piggy/SummaryViewController.m
+++ piggy/piggy/SummaryViewController.m
@@ -16,8 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
-    self.title = @"Summary";
+    // Do any additional setup after loading the view from its nib.
 }
 
 - (void)didReceiveMemoryWarning {
