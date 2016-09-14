14fe84ecde815c553392b026d4b0d44f7af10e6d
Steve Young <stephen@shopkick.com>
2016-09-13 21:26:39 -0700

    Create ExpenseDataSource

---
+++ piggy/piggy/ExpenseDataSource.h
@@ -0,0 +1,13 @@
+//
+//  ExpenseDataSource.h
+//  piggy
+//
+//  Created by Steve Young on 9/9/16.
+//  Copyright © 2016 Steve Young. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ExpenseDataSource : NSObject
+
+@end
---
+++ piggy/piggy/ExpenseDataSource.m
@@ -0,0 +1,13 @@
+//
+//  ExpenseDataSource.m
+//  piggy
+//
+//  Created by Steve Young on 9/9/16.
+//  Copyright © 2016 Steve Young. All rights reserved.
+//
+
+#import "ExpenseDataSource.h"
+
+@implementation ExpenseDataSource
+
+@end
